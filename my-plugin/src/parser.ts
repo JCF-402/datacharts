263869edc690c89a213b2f4360b7b2afe86c3205
Jomar <jcardonafuentes111@gmail.com>
2026-04-05 13:07:56 -0700

    2: Got Parser working

--- my-plugin/src/parser.ts
+++ my-plugin/src/parser.ts
@@ -1,6 +1,11 @@
 // This file contains the necessary functions to receive, PARSE, and interpret the markdown code blocks.
 // This includes intepreting and calculating the functions, limits for plotting, etc. 
-import {evaluate} from "mathjs";
+import {create , all} from "mathjs";
+
+const math = create(all);
+math.import({ // Created an alias so the user can write the more "normal" ln(x) and Mathjs wont hate me.
+    ln: math.log,
+});
 
 
 export function splitMarkdown(markdown: string) {
@@ -15,6 +20,26 @@ export type Plot = {
     color: string,
 }
 
+
+
+export function getVariable(expr: string) {
+    // Math parser is used to determine the variable in the expression.
+    // For cases where the expression is something like x^2 + G(x) + sin(x)
+    const node = math.parse(expr);
+    const vars = new Set<string>();
+
+    node.traverse(function (node: any, path: string, parent: any){
+        if (node.isSymbolNode) {
+            if (parent && parent.isFunctionNode && parent.fn === node) { //Filters out functions.
+                return
+            }
+            vars.add(node.name);
+        }
+    })
+    return [...vars];
+
+}
+
 export function getEquations(lines: string[]): string[] {
     const equations = []
     for (let line of lines) {
@@ -22,7 +47,7 @@ export function getEquations(lines: string[]): string[] {
             // Equation found. Add it to array
             const expr = line.split("=");
             if (expr[1]) {
-                equations.push(expr[1]); // Need to handle undefined later
+                equations.push(expr[1].trim()); // Need to handle undefined later
             }
         }
     }
@@ -30,13 +55,20 @@ export function getEquations(lines: string[]): string[] {
     return equations;
 }
 
+
 export function evaluateExpression(equation: string) {
-    const x_limits: [number, number, number] = [0,100,1]; // Using tuples.
+    const x_limits: [number, number, number] = [0,100,0.001]; // Using tuples. [Min, Max, Step]
     const results = [];
 
-    for (let x = x_limits[0]; x <= x_limits[1]; x += x_limits[2]) {
-        const y = evaluate(equation,{x});
-        results.push({x,y});
+    const vars = getVariable(equation);
+    const variable =  vars[0] ?? "x";
+    const compile = math.compile(equation); // Compile is apparently faster when using loops. It only needs to be generated once.
+
+
+    for (let val = x_limits[0]; val <= x_limits[1]; val += x_limits[2]) {
+        const scope = {[variable]:val}
+        const y = compile.evaluate(scope);
+        results.push({x:val,y});
     }
     return results;
 }
